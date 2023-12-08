f40e39aabf9df94cf840462da2c931a9f5b179bb
Jayden Tuam <jtuam102@gmail.com>
2023-12-08 20:03:14 +0800

    Test Push

---
+++ components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import TypeWritter from "typewritter-effect";
+
+const HeroSection = () => {
+    return(
+      <main className="py-16 mt-16 md:flex md: shirink-0">
+        <div className ="grid grid-cols-1 md:grid-cls-12">
+          <div className ="col-span-1 md:col-span-8 place-self-center jsutify-self-start">
+            <h1 className ="fold-extrabold">
+              <span className="text-white text-8xl"></span>
+            </h1>
+          </div>
+        </div>
+      </main>
+      
+    );
+}
+export default HeroSection;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TypeWritter from "typewritter-effect";
+
+const HeroSection = () => {
+    return(
+      <main>className="py-16 mt-16 md:flex"</main>
+    )
+}
+export default HeroSection;
